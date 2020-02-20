8e03be3e37f19a5947d7419f5c236cbeb9025bbf
Ankur-1980 <58996699+Ankur-1980@users.noreply.github.com>
2020-02-20 13:24:06 -0500

    made great progress
    need to figure out how to do the bonus options

---
+++ objects-2.js
@@ -0,0 +1,54 @@
+// list of characters
+const character = [
+	{ name: 'Gandalf', health: 40 },
+	{ name: 'Dumbledore', health: 50 },
+	{ name: 'Merlin', health: 25 },
+];
+// opponent
+const opponent = { name: 'Morgana', health: 100 };
+// create function to display list of all combatants
+const nameList = (...names) => {
+	for (let i of names) {
+		console.log(`${i.name}'s health is ${i.health}`);
+	}
+};
+
+// display list of all combatants
+nameList(...character);
+nameList(opponent);
+function startCombat() {
+	function pickMe() {
+		let choose = prompt(
+			`Choose a wizard to battle Morgana\nChoose between Gandalf [1], Dumbledore [2] or Merlin [3]`
+		);
+
+		choose = Number(choose);
+		// if (choose === null || choose > character.length || choose < character.length) {
+		// 	prompt(`please try again`);
+		// 	pickMe();
+		// }
+		battleChar = character[choose - 1];
+	}
+	pickMe();
+
+	while (opponent.health > 0 && battleChar.health > 0) {
+		let battleDamage = Math.floor(Math.random() * 5 + 1);
+
+		battleChar.health -= battleDamage;
+		opponent.health -= battleDamage;
+
+		if (battleChar.health > 0) {
+			console.log(`After the battle ${battleChar.name}'s health is ${battleChar.health}`);
+		} else if (battleChar.health <= 0) {
+			console.error('You Lose');
+			break;
+		}
+
+		if (opponent.health > 0) {
+			console.log(`After the battle ${opponent.name} is ${opponent.health}`);
+		} else if (opponent.health <= 0) {
+			console.error(`You Win`);
+		}
+	}
+}
+startCombat();
--- objects.js
+++ objects.js
@@ -6,54 +6,61 @@
 
 // const opponent = {name: 'Morgana', health: 75},
 
-const character = [
-	{ name: 'Gandalf', health: 40 },
-	{ name: 'Dumbledore', health: 50 },
-	{ name: 'Merlin', health: 25 },
-];
-
-const opponent = { name: 'Morgana', health: 75 };
-
-const nameList = (...names) => {
-	for (let i of names) {
-		console.log(`Name is ${i.name} and their health is ${i.health}`);
-	}
-};
-
-nameList(...character);
-nameList(opponent);
-let char;
-
 function startCombat() {
-	function chooseChar() {
-		let chooseChar = prompt(`Choose a charactern\nChoose between Gandalf [1], Dumbledore [2] or Merlin [3]`);
+	// list of characters
+	const character = [
+		{ name: 'Gandalf', health: 40 },
+		{ name: 'Dumbledore', health: 50 },
+		{ name: 'Merlin', health: 25 },
+	];
+	// opponent
+	const opponent = { name: 'Morgana', health: 100 };
+	// create function to display list of all combatants
+	const nameList = (...names) => {
+		for (let i of names) {
+			console.log(`${i.name}'s health is ${i.health}`);
+		}
+	};
+	// display list of all combtants
+	nameList(...character);
+	nameList(opponent);
 
-		if (chooseChar === null || chooseChar === '') {
-			alert('please try again');
-			chooseChar();
+	// how damage is dealt in battle
+	let battleDamage = Math.floor(Math.random() * 5 + 1);
+	char.health = pickMe(char.health) - battleDamage;
+	opponent.health = opponent.health - battleDamage;
+
+	// choosing a character for battle
+	let char = pickMe();
+	function pickMe() {
+		let pickMe = prompt(
+			'Choose a wizard to battle Morgana\nChoose between Gandalf [1], Dumbledore [2] or Merlin [3]'
+		);
+		if (pickMe === null) {
+			alert('Seriously!? Just answer the question');
+			pickMe();
 			return;
 		}
-
-		switch (chooseChar) {
+		let char;
+		switch (pickMe) {
 			case '1':
 				char = { name: 'Gandalf', health: 40 };
-				return;
+				break;
 			case '2':
 				char = { name: 'Dumbledore', health: 50 };
-				return;
+				break;
 			case '3':
 				char = { name: 'Merlin', health: 25 };
-				return;
+				break;
 		}
-		chooseChar();
+		console.log(`${char.name} has ${char.health} health`);
+		return char;
 	}
 
-	let battleDamage = Math.floor(Math.random() * 5 + 1);
-
-	let heroesHP = chooseChar(char.health) - battleDamage;
-	let opponentHP = opponent.health - battleDamage;
-
-	while (opponentHP > 0) console.warn(`${chooseChar(char.name)}'s health is ${heroesHP}`);
-	console.error(`${opponent.name}'s health is ${opponentHP}`);
+	console.log(`After the battle ${char.name}'s health is ${char.health}`);
+	console.log(`After the battle ${opponent.name} is ${opponent.health}`);
 }
+
 startCombat();
+
+// loop for battle
