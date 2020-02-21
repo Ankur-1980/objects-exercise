17cb597fa5666b88bd4afbfb0ba4ee02d4751a0e
Ankur-1980 <58996699+Ankur-1980@users.noreply.github.com>
2020-02-20 19:31:01 -0500

    updated code

--- objects-2.js
+++ objects-2.js
@@ -5,7 +5,7 @@ let character = [
 	{ name: 'Merlin', health: 12 },
 ];
 // opponent
-const opponent = { name: 'Morgana', health: 50 };
+const opponent = { name: 'Morgana', health: 70 };
 // create function to display list of all combatants
 const nameList = (...names) => {
 	for (let i of names) {
@@ -16,22 +16,24 @@ const nameList = (...names) => {
 // display list of all combatants
 nameList(...character);
 nameList(opponent);
+//  parseInt("string", 10)
 function startCombat() {
 	function pickMe() {
-		let choose = prompt(
-			`Choose a wizard to battle Morgana\nChoose between \n${character[0].name} [1], \n${character[1].name} [2] or \n${character[2].name} [3]`
-			// `${nameList(...character)}`
+		let choose = parseInt(
+			prompt(
+				`Choose a wizard to battle Morgana\nChoose between \n${character[0].name} [1], \n${character[1].name} [2] or \n${character[2].name} [3]`
+				// `${nameList(...character)}`
+			),
+			10
 		);
 
-		choose = Number(choose);
-		// why is this alerting "Run away coward"???
 		// also when I enter a wrong number, then a correct one, it breaks the code.
 
-		if (choose === '' || choose > character.length || choose <= 0) {
-			alert(`please try again`);
+		if (choose === '' || choose > character.length) {
+			// alert(`please try again`);
 			pickMe();
-		} else if (choose === null) {
-			console.warn(`Run away cowards`);
+		} else if (choose === 0) {
+			alert(`Run away cowards`);
 		}
 
 		battleChar = character[choose - 1];
@@ -39,7 +41,7 @@ function startCombat() {
 	}
 	pickMe();
 	// can't figure out how to keep the loop going after a character has died I feel like I'm on the right track.
-	while (opponent.health > 0 && character.every(x => x <= 0) === false) {
+	while (opponent.health > 0 && !character.every(x => x.health <= 0)) {
 		let battleDamage = Math.floor(Math.random() * 5 + 1);
 
 		battleChar.health -= battleDamage;
@@ -52,11 +54,13 @@ function startCombat() {
 		if (battleChar.health > 0) {
 			console.log(`After the battle ${battleChar.name}'s health is ${battleChar.health}`);
 			pickMe();
+		} else if (character.every(x => x.health <= 0)) {
+			console.error(`You lose`);
 		} else if (battleChar.health <= 0) {
 			console.error(`${battleChar.name} has died.`);
 			character.splice(character.indexOf(battleChar), 1, battleChar);
-		} else if (character.every(x => x <= 0) === true) {
-			console.error(`You lose`);
+			// Array.splice(start index, number of deletions, what is going in )
+			pickMe();
 		}
 
 		if (opponent.health > 0) {
