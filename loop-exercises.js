dd29649fc4db02bf7de1c26d80931d544679bb85
Ankur-1980 <58996699+Ankur-1980@users.noreply.github.com>
2020-02-23 17:40:38 -0500

    started loop exercises

---
+++ loop-exercises.js
@@ -0,0 +1,25 @@
+// // Count to twenty
+// for (let twenty = 0; twenty <= 20; twenty++) {
+// 	console.log(twenty);
+// }
+
+// Evens up to twenty
+// for (let twenty = 0; twenty <= 20; twenty++) {
+// 	if (twenty % 2 === 0) {
+// 		console.log(twenty);
+// 	}
+// }
+
+// // Odds up to twenty
+// for (let twenty = 0; twenty <= 20; twenty++) {
+// 	if (twenty % 2 === 1) {
+// 		console.log(twenty);
+// 	}
+// }
+
+// Count from 1 -10
+// Multiply each iteration by 3
+for (let timesThree = 0; timesThree <= 10; timesThree++) {
+	timesThree = timesThree * 3;
+	console.log(timesThree);
+}
