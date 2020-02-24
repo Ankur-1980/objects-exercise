a9990241b25c9e101cb7b969b839a3af7aa0175b
Ankur-1980 <58996699+Ankur-1980@users.noreply.github.com>
2020-02-24 18:41:42 -0500

    couple exercises from W3resource

---
+++ loops-exercises/loops.js
@@ -0,0 +1,10 @@
+// // larger number
+// // prompt for two numbers
+// // display the larger one
+
+// const larger = (a, b) => (a > b ? a : b);
+// console.log(larger(24, 34));
+
+// // display if product is a positive or negative number
+// const posNeg = (a, b, c) => (a * b * c >= 0 ? 'positive' : 'negative');
+// console.log(posNeg(4, 1, 5));
